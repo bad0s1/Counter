6bcd7f7cdaba15c460a4f5b32a91368ebe1446bd
bad0s <dosadin@gmail.com>
2025-10-18 23:37:26 +0300

    Corrected on the recommendation of the reviewer - class maked as final, var & method are made private.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -1,28 +1,18 @@
-//
-//  ViewController.swift
-//  Counter
-//
-//  Created by Alexander Dosadin on 18.10.2025.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
-    @IBOutlet weak var counterValue: UILabel!
+   @IBOutlet private weak var counterValue: UILabel!
     
     private var counter = 0
     
-    @IBAction func buttonTapped(_ sender: Any) {
+   @IBAction private func buttonTapped(_ sender: Any) {
         counter += 1
         counterValue.text = "Counter value: \(counter)"
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         counterValue.text = "Counter value: \(counter)"
     }
-
-
 }
 
